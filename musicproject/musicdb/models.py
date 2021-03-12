7c4bc63e24728f2273674501ef91c0f199e0737c
matthewquerdasi <matthew.querdasi@gmail.com>
2021-03-11 20:41:03 -0500

    models for users, ratings, and artists

--- musicproject/musicdb/models.py
+++ musicproject/musicdb/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    username = models.CharField(max_length=50, primary_key=True)
+    password = models.CharField(max_length=50)
+
+class Artists(models.Model):
+    song = models.CharField(max_length=50, primary_key=True)
+    artist = models.CharField(max_length=200)
+
+class Ratings(models.Model):
+    username = models.ForeignKey(Users, primary_key=True, unique=True, on_delete=models.CASCADE)
+    song = models.ForeignKey(Artists, on_delete=models.CASCADE)
+    rating = models.IntegerField(default=0)
