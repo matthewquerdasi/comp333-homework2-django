d8c942ef6b66251e208da4abdb0e7edb0352fe9c
Tommy Alpert <tommy0356@gmail.com>
2021-04-18 21:15:48 -0400

    added song querying by title and artist

--- musicproject/musicdb/views.py
+++ musicproject/musicdb/views.py
@@ -4,15 +4,41 @@ from django.template import loader
 from musicdb.forms import UserForm, RetrieveRatingsForm, RetrieveByYearForm
 from musicdb.models import Users, Ratings, Years, Songs
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from musicdb.serializers import UserSerializer, SongSerializer, RatingSerializer, YearSerializer
 
 class UserView (viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = Users.objects.all()
 
-class SongView (viewsets.ModelViewSet):
+class SongView(viewsets.ModelViewSet):
     serializer_class = SongSerializer
     queryset = Songs.objects.all()
+
+    @action(detail=True)
+    def songs_by_artist(self, request, pk=None):
+      artist_songs = Songs.objects.filter(artist=pk)
+      page = self.paginate_queryset(artist_songs)
+
+      if page is not None:
+          serializer = self.get_serializer(page, many=True)
+          return self.get_paginated_response(serializer.data)
+
+      serializer = self.get_serializer(artist_songs, many=True)
+      return Response(serializer.data)
+
+    @action(detail=True)
+    def songs_by_title(self, request, pk=None):
+      title_songs = Songs.objects.filter(song=pk)
+      page = self.paginate_queryset(title_songs)
+
+      if page is not None:
+          serializer = self.get_serializer(page, many=True)
+          return self.get_paginated_response(serializer.data)
+
+      serializer = self.get_serializer(title_songs, many=True)
+      return Response(serializer.data)
 
 class RatingView (viewsets.ModelViewSet):
     serializer_class = RatingSerializer
