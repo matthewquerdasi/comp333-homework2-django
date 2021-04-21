ef3d54caa0d3a1612593e531e45bf2fd486e8afe
matthewquerdasi <matthew.querdasi@gmail.com>
2021-04-20 22:16:42 -0400

    fixed merge conflicts

--- musicproject/musicdb/views.py
+++ musicproject/musicdb/views.py
@@ -13,7 +13,6 @@ class UserView (viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = Users.objects.all()
 
-<<<<<<< HEAD
 
 class SongView (viewsets.ModelViewSet):
     serializer_class = SongSerializer
@@ -22,35 +21,6 @@ class SongView (viewsets.ModelViewSet):
     def put(self, request, *args, **kwargs):
         return(self.update(request, *args, **kwargs))
 
-=======
-class SongView(viewsets.ModelViewSet):
-    serializer_class = SongSerializer
-    queryset = Songs.objects.all()
-
-    @action(detail=True)
-    def songs_by_artist(self, request, pk=None):
-      artist_songs = Songs.objects.filter(artist=pk)
-      page = self.paginate_queryset(artist_songs)
-
-      if page is not None:
-          serializer = self.get_serializer(page, many=True)
-          return self.get_paginated_response(serializer.data)
-
-      serializer = self.get_serializer(artist_songs, many=True)
-      return Response(serializer.data)
-
-    @action(detail=True)
-    def songs_by_title(self, request, pk=None):
-      title_songs = Songs.objects.filter(song=pk)
-      page = self.paginate_queryset(title_songs)
-
-      if page is not None:
-          serializer = self.get_serializer(page, many=True)
-          return self.get_paginated_response(serializer.data)
-
-      serializer = self.get_serializer(title_songs, many=True)
-      return Response(serializer.data)
->>>>>>> d8c942ef6b66251e208da4abdb0e7edb0352fe9c
 
 class RatingView (viewsets.ModelViewSet):
     serializer_class = RatingSerializer
