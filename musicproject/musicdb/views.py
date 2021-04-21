c41fa875bff016fb9b9a8970a8859dfc0e3645f7
matthewquerdasi <matthew.querdasi@gmail.com>
2021-04-20 22:13:34 -0400

    small views change, should not affect functionality

--- musicproject/musicdb/views.py
+++ musicproject/musicdb/views.py
@@ -6,24 +6,33 @@ from musicdb.models import Users, Ratings, Years, Songs
 from rest_framework import viewsets
 from musicdb.serializers import UserSerializer, SongSerializer, RatingSerializer, YearSerializer
 
+
 class UserView (viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = Users.objects.all()
+
 
 class SongView (viewsets.ModelViewSet):
     serializer_class = SongSerializer
     queryset = Songs.objects.all()
 
+    def put(self, request, *args, **kwargs):
+        return(self.update(request, *args, **kwargs))
+
+
 class RatingView (viewsets.ModelViewSet):
     serializer_class = RatingSerializer
     queryset = Ratings.objects.all()
+
 
 class YearView (viewsets.ModelViewSet):
     serializer_class = YearSerializer
     queryset = Years.objects.all()
 
+
 def index(request):
     return render(request, 'index.html')
+
 
 def user_create(request):
     if request.method == 'POST':
@@ -38,13 +47,15 @@ def user_create(request):
 
 def retrieve_ratings(request):
     form = RetrieveRatingsForm(request.GET or None)
-    ratings = None 
+    ratings = None
     if request.method == 'GET':
         if form.is_valid():
             # return HttpResponse("POST SUCCESSFUL")
-            ratings = Ratings.objects.filter(username=form.cleaned_data.get('username'))
-            
+            ratings = Ratings.objects.filter(
+                username=form.cleaned_data.get('username'))
+
     return render(request, 'retrieve_ratings.html', {'form': form, 'ratings': ratings})
+
 
 def retrieve_years(request):
     form = RetrieveByYearForm(request.GET or None)
@@ -54,5 +65,3 @@ def retrieve_years(request):
             year = Years.objects.filter(year=form.cleaned_data.get('year'))
 
     return render(request, 'retrieve_years.html', {'form': form, 'year': year})
-
-
